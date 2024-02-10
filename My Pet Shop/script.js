55f2aad9d0e5cb3d281b96161828ab13bcd1501d
SOAM PARKASH <145873955+soamparkash@users.noreply.github.com>
2024-02-10 19:54:41 +0530

    Add files via upload

---
+++ My Pet Shop/script.js
@@ -0,0 +1,40 @@
+let login = document.querySelector(".for-login");
+
+document.querySelector("#login-btn").onclick = () =>{
+    login.classList.toggle('active');
+    navbar.classList.remove('active');
+}
+
+let navbar = document.querySelector(".header .navbar");
+
+document.querySelector('#menu-btn').onclick = () =>{
+    login.classList.remove('active');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+    login.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
+var swiper = new Swiper(".gallery-slider", {
+    spaceBetween: 20,
+    centeredSlides: true,
+    grabCursor: true,
+    autoplay:{
+    delay: 1500,
+    disableOnInteraction: false,
+  },
+  loop: true,
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    991: {
+      slidesPerView: 3,
+    },
+  },
+})
